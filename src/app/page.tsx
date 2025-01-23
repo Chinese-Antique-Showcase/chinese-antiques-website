7a480cbe24b478ff697ead82dcf68e90d1ce52b7
Johnny Yang <jyang8501@gmail.com>
2025-01-22 22:15:07 -0500

    Add basic strapi data retrieval

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,46 @@
+import mockData from "../../mock-data.json"
 
-export default function Home() {
+// Server Component Example
+
+async function getAntiqueData() {
+  const res = await fetch(`${process.env.STRAPI_URL}/api/antiques?populate=*`, {
+    cache: "force-cache", // Cache the data if it doesn't update often
+    headers: {
+      AUthorization: `Bearer ${process.env.API_BEARER_TOKEN}`
+    }
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+function getAntiqueDataMock()
+{
+  return mockData;
+}
+
+
+export default async function Home() {
+
+  const responseMock = getAntiqueDataMock();
+  const antiquesMock = responseMock.data;
+
+
+  const response = await getAntiqueData();
+  const antiques = response.data;
+
   return (
+
+    // Replace antiques with antiquesMock
     <div>
-        <div>test</div>
+      <ul>
+        {antiques.map((item) => (
+            <li key={item.id}>{item.Name}</li>
+          ))}
+      </ul>
     </div>
   );
 }
