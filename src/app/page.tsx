5e8fbb2933ca292ccf61f97fb909034f2247e94b
Johnny Yang <jyang8501@gmail.com>
2025-02-11 07:30:20 -0500

    Test strapi data fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,45 @@
-import mockData from "../../mock-data.json"
+// import mockData from "../../mock-data.json"
 
 // Server Component Example
 
-// async function getAntiqueData() {
-//   const res = await fetch(`${process.env.STRAPI_URL}/api/antiques?populate=*`, {
-//     cache: "force-cache", // Cache the data if it doesn't update often
-//     headers: {
-//       AUthorization: `Bearer ${process.env.API_BEARER_TOKEN}`
-//     }
-//   });
+async function getAntiqueData() {
+  const res = await fetch(`${process.env.STRAPI_URL}/api/antiques?populate=*`, {
+    cache: "force-cache", // Cache the data if it doesn't update often
+    headers: {
+      AUthorization: `Bearer ${process.env.API_BEARER_TOKEN}`
+    }
+  });
 
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch data");
-//   }
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-//   return res.json();
-// }
-
-function getAntiqueDataMock()
-{
-  return mockData;
+  return res.json();
 }
 
+// function getAntiqueDataMock()
+// {
+//   return mockData;
+// }
+
+interface Antique {
+  id: number; // Assuming 'id' is a number
+  Name: string;
+  Description: string;
+  TimePeriod: string;
+  MainPhoto: string | null; 
+  AdditionalPhotos: string[] | null; 
+  Price: number;
+}
 
 export default async function Home() {
 
-  const responseMock = getAntiqueDataMock();
-  const antiquesMock = responseMock.data;
+  // const responseMock = getAntiqueDataMock();
+  // const antiquesMock = responseMock.data;
 
 
-  // const response = await getAntiqueData();
-  // const antiques = response.data;
+  const response = await getAntiqueData();
+  const antiques = response.data;
 
   //test comment on right branch
   return (
@@ -38,7 +47,7 @@ export default async function Home() {
     // Replace antiques with antiquesMock
     <div>
       <ul>
-        {antiquesMock.map((item) => (
+        {antiques.map((item : Antique) => (
             <li key={item.id}>{item.Name}</li>
           ))}
       </ul>
