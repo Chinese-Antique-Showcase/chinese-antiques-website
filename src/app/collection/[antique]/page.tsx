3aeaf021ffb1dc5adc021d83195c4e0592fda2fb
Johnny Yang <jyang8501@gmail.com>
2025-02-23 08:35:17 -0500

    Keep route static and refresh upon strapi changes

--- src/app/collection/[antique]/page.tsx
+++ src/app/collection/[antique]/page.tsx
@@ -4,7 +4,11 @@ import { AdditionalPhoto, DisplayAntique, IDItem } from "@/_data/antique";
 // TODO - change to create own 404 page / look into more
 // A configuration value to denote whether or not to generate pages that exist
 // (https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamicparams)
-export const dynamicParams = false;
+
+
+//Force page to render statically (except images)
+export const dynamic = "force-static"; //Also sets: export const dynamicParams = false;  
+
 export async function generateStaticParams() {
   const response = await fetch(
     `${process.env.STRAPI_URL}/api/antiques?fields=id`,
